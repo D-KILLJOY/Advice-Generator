5061d0c181fde631cff7d31ec5b7f2aef1b0e9b5
Uchenna Didia <didiauche@gmail.com>
2023-05-24 17:31:23 +0100

    fixed firefox not generating new advice

--- app.js
+++ app.js
@@ -2,14 +2,16 @@ const adviceId = document.querySelector(".advice-id");
 const adviceText = document.querySelector(".advice-text");
 const adviceGenerate = document.querySelector(".dice");
 
-async function advice() {
-	const Response = await fetch("https://api.adviceslip.com/advice");
-
+async function advice(slip_id) {
+	const Response = await fetch(`https://api.adviceslip.com/advice/${slip_id}`);
 	const Data = await Response.json();
-
 	adviceId.textContent = Data.slip.id;
 	adviceText.textContent = Data.slip.advice;
 }
 
-advice();
-adviceGenerate.addEventListener("click", advice);
+advice(Math.floor(Math.random() * 224));
+adviceGenerate.addEventListener("click", () => {
+	const num = Math.floor(Math.random() * 224);
+
+	advice(num);
+});
